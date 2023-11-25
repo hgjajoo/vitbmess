6dae13d5cfef00da6a12fbe753d22a99a69a03f9
Harshvardhan J <63530396+hgjajoo@users.noreply.github.com>
2023-11-25 11:47:58 +0530

    update day time v1.3.1

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-function updateMealTimetable() {
-    var selectedDay = document.getElementById("day").value;
-    var selectedMeal = document.getElementById("meal").value;
-    var mealItems = getMealTimetable(selectedDay, selectedMeal);
-
-    var mealTimetableDiv = document.getElementById("mealTimetable");
-    mealTimetableDiv.innerHTML = "<strong>" + selectedDay + " - " + selectedMeal + ":</strong>";
-
-    if (mealItems && mealItems.length > 0) {
-        var itemList = "<ul>";
-        mealItems.forEach(function (item) {
-            itemList += "<li>" + item + "</li>";
-        });
-        itemList += "</ul>";
-        mealTimetableDiv.innerHTML += itemList;
-    } else {
-        mealTimetableDiv.innerHTML += "No items added for this time.";
-    }
-}
-
 function getMealTimetable(day, meal) {
     var timetableData = {
         Monday: {
@@ -66,6 +46,79 @@ function getMealTimetable(day, meal) {
     };
 
     return timetableData[day][meal] || [];
+}
+
+function updateMealTimetable() {
+    var selectedDay = document.getElementById("day").value;
+    var selectedMeal = document.getElementById("meal").value;
+    var mealItems = getMealTimetable(selectedDay, selectedMeal);
+
+    var mealTimetableDiv = document.getElementById("mealTimetable");
+    mealTimetableDiv.innerHTML = "<strong>" + selectedDay + " - " + selectedMeal + ":</strong>";
+
+    if (mealItems && mealItems.length > 0) {
+        var itemList = "<ul>";
+        mealItems.forEach(function (item) {
+            itemList += "<li>" + item + "</li>";
+        });
+        itemList += "</ul>";
+        mealTimetableDiv.innerHTML += itemList;
+    } else {
+        mealTimetableDiv.innerHTML += "No items added yet, will be updated soon!";
+    }
+}
+
+// sets default day
+document.addEventListener('DOMContentLoaded', function() {
+    setDefaultMeal();
+});
+//conditions for default day/time
+function setDefaultMeal() {
+    var currentDate = new Date();
+    var currentDay = currentDate.toLocaleString('en-us', { weekday: 'long' });
+    var currentHour = currentDate.getHours();
+    var defaultDay = "";
+    var defaultMeal = "";
+
+    switch (currentDay) {
+        case 'Monday':
+            defaultDay = 'Monday';
+            break;
+        case 'Tuesday':
+            defaultDay = 'Tuesday';
+            break;
+        case 'Wednesday':
+            defaultDay = 'Wednesday';
+            break;
+        case 'Thursday':
+            defaultDay = 'Thursday';
+            break;
+        case 'Friday':
+            defaultDay = 'Friday';
+            break;
+        case 'Saturday':
+            defaultDay = 'Saturday';
+            break;
+        case 'Sunday':
+            defaultDay = 'Sunday';
+            break;    
+        default:
+            defaultDay = 'Monday'; 
+    }
+
+    if (currentHour >= 0 && currentHour < 10) {
+        defaultMeal = "Breakfast";
+    } else if (currentHour >= 10 && currentHour < 15) {
+        defaultMeal = "Lunch";
+    } else if (currentHour >= 15 && currentHour < 17) {
+        defaultMeal = "Snacks";
+    } else if (currentHour >= 17 && currentHour < 24) {
+        defaultMeal = "Dinner";
+    }
+
+    document.getElementById("day").value = defaultDay;
+    document.getElementById("meal").value = defaultMeal;
+    updateMealTimetable();
 }
 
 /*
